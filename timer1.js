ca187afeb7c8ccb350c4e451c9d4d90f25e8749a
Jordan <jordan.benson4@gmail.com>
2022-01-18 13:50:50 -0800

    Added timer1.js

---
+++ timer1.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2);
+
+for (const input of args) {
+  if (args.length === 0 || input < 0 || !Number(input)) {
+    return;
+  }
+  const sec = Number(input);
+  setTimeout(() => {
+    process.stdout.write('\x07');
+    console.log('hi');
+  }, sec * 1000);
+}
